884bcd4a38bee930d0716b50cdcc629b9eda49df
bei huang <beihuang@iastate.edu>
2009-04-01 18:29:40 +0000

    Add speed control

--- R/gui_andrews.r
+++ R/gui_andrews.r
@@ -16,6 +16,7 @@ vbox = glayout(cont=w)
 type = "Grand"
 VarIndex=c(1,2)
 ClIndex <<- c(1:length(x))
+speed_aps = 1
 
 
 #=====================split data set===================================
@@ -50,17 +51,17 @@ getTourType = function (h,...)
 displayTour = function (h,...)
 {
 	if (type == "Grand")
- 		animate_andrews(x1[VarIndex],grand_tour())
+ 		animate_andrews(x1[VarIndex],grand_tour(),aps = speed_aps)
 	if (type == "Little")
- 		animate_andrews(x1[VarIndex],little_tour())
+ 		animate_andrews(x1[VarIndex],little_tour(),aps = speed_aps)
 	if (type == "Guided(holes)")
-		animate_andrews(x1[VarIndex],guided_tour(holes))
+		animate_andrews(x1[VarIndex],guided_tour(holes),aps = speed_aps)
 	if (type == "Guided(cm)") 
-		animate_andrews(x1[VarIndex],guided_tour(cm))
+		animate_andrews(x1[VarIndex],guided_tour(cm),aps = speed_aps)
 	if (type == "Guided(lda_pp)") 
-		animate_andrews(x1[VarIndex],guided_tour(lda_pp,cl=cl))
+		animate_andrews(x1[VarIndex],guided_tour(lda_pp,cl=cl),aps = speed_aps)
 	if (type == "Local") 
- 		animate_andrews(x1[VarIndex],local_tour(basis_init(length(VarIndex), 2)))
+ 		animate_andrews(x1[VarIndex],local_tour(basis_init(length(VarIndex), 2)),aps = speed_aps)
 }
 #===============================================
 
@@ -88,6 +89,11 @@ TourType = gradio(short, cont=vbox, handler = NULL)
 addHandlerChanged(TourType,handler = getTourType)
 
 vbox[2,3] <- TourType
+
+# speed slider control
+vbox[3,1, anchor=c(-1,0)] <- "Speed"
+vbox[4,1, expand=T] <- (sl <- gslider(from = 0, to= 10, by=0.1, value = 1, 
+  	cont = vbox, handler = function(h,...){speed_aps <<- svalue(h$obj)}))
 
 
 # buttons control
