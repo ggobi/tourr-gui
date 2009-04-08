c73f40c2616f97947adf0172a640abe45cd486ac
Barret Schloerke <bigbear@iastate.edu>
2009-04-08 19:39:40 +0000

    made the xy gui allow for quiting, pausing, and opening a cairo device on a mac

--- R/gui_xy.r
+++ R/gui_xy.r
@@ -36,21 +36,21 @@ gui_xy = function(x = flea, ...) {
   # Handler of Control 1
   getVariables = function (h,...)
   {
-  	VarIndex <<-svalue(Variables, index = T)
+    VarIndex <<-svalue(Variables, index = T)
   }
 
   # Handler of Control 2
   getTourType = function (h,...)
   {
-  	type <<-svalue(TourType)
+    type <<-svalue(TourType)
   }
 
 
   # Handler of Class
   getClass = function (h,...)
   {
-  	ClIndex <<-svalue(h$obj, index = T)
-  	ColType <<- rainbow_hcl(length(unique(x2[,ClIndex])))
+    ClIndex <<-svalue(h$obj, index = T)
+    ColType <<- rainbow_hcl(length(unique(x2[,ClIndex])))
   }
 
 
@@ -63,30 +63,30 @@ gui_xy = function(x = flea, ...) {
 
   displayTour = function (h,...)
   {
-  	if (type == "Grand" & length(ClIndex) == length(x))
-   		animate_xy(x1[VarIndex],grand_tour(), center=T,aps = speed_aps, axes = axes_location)
-  	if (type == "Grand" & length(ClIndex) != length(x))		
-  		animate_xy(x1[VarIndex],grand_tour(), center=T,aps = speed_aps, axes = axes_location, col=ColType[as.numeric(x2[,ClIndex])])
-  	if (type == "Little" & length(ClIndex) == length(x))
-   		animate_xy(x1[VarIndex],little_tour(), center=T,aps = speed_aps, axes = axes_location)
-  	if (type == "Little"& length(ClIndex) != length(x))
-  		animate_xy(x1[VarIndex],little_tour(), aps = speed_aps, axes = axes_location, col=ColType[as.numeric(x2[,ClIndex])])
-  	if (type == "Guided(holes)" & length(ClIndex) == length(x))
-  		animate_xy(x1[VarIndex],guided_tour(holes), aps = speed_aps, axes = axes_location)
-  	if (type == "Guided(holes)" & length(ClIndex) != length(x))
-  		animate_xy(x1[VarIndex],guided_tour(holes), aps = speed_aps, axes = axes_location, col=ColType[as.numeric(x2[,ClIndex])])
-  	if (type == "Guided(cm)" & length(ClIndex) == length(x))
-  		animate_xy(x1[VarIndex],guided_tour(cm), aps = speed_aps, axes = axes_location)
-  	if (type == "Guided(cm)" & length(ClIndex) != length(x))
-  		animate_xy(x1[VarIndex],guided_tour(cm), aps = speed_aps, axes = axes_location, col=ColType[as.numeric(x2[,ClIndex])])
-  	if (type == "Guided(lda_pp)" & length(ClIndex) == length(x)) 
-  		animate_xy(x1[VarIndex],guided_tour(lda_pp,cl=cl), aps = speed_aps, axes = axes_location)
-  	if (type == "Guided(lda_pp)" & length(ClIndex) != length(x)) 
-  		animate_xy(x1[VarIndex],guided_tour(lda_pp,cl=cl), aps = speed_aps, axes = axes_location,col=ColType[as.numeric(x2[,ClIndex])])
-  	if (type == "Local" & length(ClIndex) == length(x))
-   		animate_xy(x1[VarIndex],local_tour(basis_init(length(VarIndex), 2)), center=T,aps = speed_aps, axes = axes_location)
-  	if (type == "Local"& length(ClIndex) != length(x))
-  		animate_xy(x1[VarIndex],local_tour(basis_init(length(VarIndex), 2)), aps = speed_aps, axes = axes_location, col=ColType[as.numeric(x2[,ClIndex])])	
+    if (type == "Grand" & length(ClIndex) == length(x))
+       animate_xy(x1[VarIndex],grand_tour(), center=T,aps = speed_aps, axes = axes_location)
+    if (type == "Grand" & length(ClIndex) != length(x))    
+      animate_xy(x1[VarIndex],grand_tour(), center=T,aps = speed_aps, axes = axes_location, col=ColType[as.numeric(x2[,ClIndex])])
+    if (type == "Little" & length(ClIndex) == length(x))
+       animate_xy(x1[VarIndex],little_tour(), center=T,aps = speed_aps, axes = axes_location)
+    if (type == "Little"& length(ClIndex) != length(x))
+      animate_xy(x1[VarIndex],little_tour(), aps = speed_aps, axes = axes_location, col=ColType[as.numeric(x2[,ClIndex])])
+    if (type == "Guided(holes)" & length(ClIndex) == length(x))
+      animate_xy(x1[VarIndex],guided_tour(holes), aps = speed_aps, axes = axes_location)
+    if (type == "Guided(holes)" & length(ClIndex) != length(x))
+      animate_xy(x1[VarIndex],guided_tour(holes), aps = speed_aps, axes = axes_location, col=ColType[as.numeric(x2[,ClIndex])])
+    if (type == "Guided(cm)" & length(ClIndex) == length(x))
+      animate_xy(x1[VarIndex],guided_tour(cm), aps = speed_aps, axes = axes_location)
+    if (type == "Guided(cm)" & length(ClIndex) != length(x))
+      animate_xy(x1[VarIndex],guided_tour(cm), aps = speed_aps, axes = axes_location, col=ColType[as.numeric(x2[,ClIndex])])
+    if (type == "Guided(lda_pp)" & length(ClIndex) == length(x)) 
+      animate_xy(x1[VarIndex],guided_tour(lda_pp,cl=cl), aps = speed_aps, axes = axes_location)
+    if (type == "Guided(lda_pp)" & length(ClIndex) != length(x)) 
+      animate_xy(x1[VarIndex],guided_tour(lda_pp,cl=cl), aps = speed_aps, axes = axes_location,col=ColType[as.numeric(x2[,ClIndex])])
+    if (type == "Local" & length(ClIndex) == length(x))
+       animate_xy(x1[VarIndex],local_tour(basis_init(length(VarIndex), 2)), center=T,aps = speed_aps, axes = axes_location)
+    if (type == "Local"& length(ClIndex) != length(x))
+      animate_xy(x1[VarIndex],local_tour(basis_init(length(VarIndex), 2)), aps = speed_aps, axes = axes_location, col=ColType[as.numeric(x2[,ClIndex])])  
   }
   #===============================================
 
@@ -125,31 +125,45 @@ gui_xy = function(x = flea, ...) {
   # speed slider control
   vbox[5,1, anchor=c(-1,0)] <- "Speed"
   vbox[6,1, expand=T] <- (sl <- gslider(from = 0, to= 10, by=0.1, value = 1, 
-  	cont = vbox, handler = function(h,...){speed_aps <<- svalue(h$obj)}))
+    cont = vbox, handler = function(h,...){speed_aps <<- svalue(h$obj)}))
 
 
   #axes control
   vbox[3,3, anchor=c(-1,0)] <- "Axes Locations"
   Location=c("center", "bottomleft", "off")
   vbox[4,3, anchor=c(-1,0)]<-(dl<-gradio(Location,cont=vbox,
-  			handler=function(h,...){axes_location <<-svalue(h$obj)}))
+    handler=function(h,...){axes_location <<-svalue(h$obj)}))
 
 
   # buttons control
 
   buttonGroup = ggroup(horizontal = F, cont=vbox)
-  pauseButton = gbutton("Pause",cont=buttonGroup)
+  
+  pauseButton = gcheckbox("Pause",cont=buttonGroup, handler = function(h,...){
+    pausePushed <<- svalue(h$obj)
+  }) 
   addSpace(buttonGroup,10)
 
+  okButton = gbutton("Apply", cont=buttonGroup)
+  addhandlerclicked(okButton, handler = function(h,...) {
+    svalue(pauseButton) <- FALSE  
+    displayTour()
+   } )
+  addSpace(buttonGroup,10)
+
+  quitPushed <<- FALSE
   quitButton = gbutton("Quit",cont=buttonGroup)
-  addHandlerClicked(quitButton, handler= function(h,...) dispose(w))
-  addSpace(buttonGroup,10)
-
-  okButton = gbutton("ok", cont=buttonGroup)
-  addHandlerClicked(okButton, handler = displayTour)
+  addHandlerClicked(quitButton, handler= function(h,...){
+    quitPushed <<- TRUE
+    dispose(w)
+    dev.off()
+  })
 
   vbox[4,4, anchor=c(0,1)] = buttonGroup
-
+  
+  if(length(strsplit(R.Version()$os,"darwin")[[1]]) > 1){
+    require(Cairo)
+    library(Cairo)
+    CairoX11()
+  }
 }
-
-
