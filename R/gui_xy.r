128b799018471527ad3c0793b61c1ae560ab17a4
Hadley Wickham <hadley@rice.edu>
2009-05-14 15:09:55 +0000

    More gui tweaks

--- R/gui_xy.r
+++ R/gui_xy.r
@@ -1,55 +1,20 @@
 library(colorspace)
-library(gWidgets)
 library(RGtk2)
-options("guiToolkit"="RGtk2")
+library(gWidgets)
 
 gui_xy <- function(data = flea, ...) {
   num <- sapply(data, is.numeric)
-
-  create_tour <- function(h, ...) {
-    var_selected <- svalue(Variables)
-    if (length(var_selected) < 3) {
-      gmessage("Please select at least three variables", icon = "warning")
-      return()
-    }
-    
-    # Work out point colours
-    cat_selected <- data[svalue(Class)]
-    if (length(cat_selected) > 0) {
-      # collapse to single variable if multiple selected
-      int <- interaction(cat_selected, drop = TRUE)
-      pal <- rainbow_hcl(length(levels(int)))
-      col <- pal[as.numeric(int)]
-    } else {
-      col <- "black"
-    }
-    axes_location <- svalue(dl)
-    display <- display_xy(data, axes = axes_location, center = TRUE, 
-      col = col)
-
-    # Work out which type of tour to use
-    tour <- switch(svalue(TourType),
-      "Grand" = grand_tour(), 
-      "Little" = little_tour(), 
-      "Guided(holes)" = guided_tour(holes), 
-      "Guided(cm)" = guided_tour(cm), 
-      "Guided(lda_pp)" = guided_tour(), 
-      "Local" = local_tour()
-    )
-    
-    aps <- svalue(sl)
-        
-    list(
-      data = rescale(data[var_selected]),
-      tour_path = tour,
-      display = display,
-      aps = aps
-    )
-  }
+  
   tour <- NULL
   tour_anim <- NULL
   update_tour <- function(...) {
-    tour <<- create_tour(...)
+    tour <<- create_tour(data,
+      var_selected = svalue(Variables), 
+      cat_selected = svalue(Class), 
+      axes_location = svalue(dl),
+      tour_type = svalue(TourType),
+      aps = svalue(sl)
+    )
     tour_anim <<- with(tour, tourer(data, tour_path, velocity = aps / 33))
     
     tour$display$init(tour$data)
@@ -59,8 +24,8 @@ gui_xy <- function(data = flea, ...) {
   }
   
   draw_frame <- function(...) {
-    if (is.null(tour)) return(TRUE)  # if there's no tour, exit
-    if (svalue(pauseButton)) return(TRUE) # if the tour is paused, exit
+    # if there's no tour, don't draw anything
+    if (is.null(tour)) return(TRUE)  
 
     tour_step <- tour_anim$step2(svalue(sl) / 33)
     tour$display$render_transition()
@@ -71,76 +36,55 @@ gui_xy <- function(data = flea, ...) {
   }
   
   
-
   # ==================Controls==========================
-  w = gwindow("2D Tour plot example")
-  g = ggroup(cont = w, horizontal = FALSE)
-
-  if(options("guiToolkit") == "RGtk2") {
-     cont = w
-   } else {
-     cont = g
-   }
-
-  vbox = glayout(cont=w)
+  w <- gwindow("2D Tour plot example", visible = FALSE)
+  vbox <- glayout(cont = w)
 
   # Variable selection column
   vbox[1, 1, anchor = c(-1, 0)] <- "Variable Selection"
   vbox[2, 1] <- Variables <- gcheckboxgroup(names(data[num]), 
-    checked = TRUE, horizontal = FALSE, cont=vbox)
+    checked = TRUE, horizontal = FALSE)
   vbox[3, 1, anchor = c(-1, 0)] <- "Class Selection"
   vbox[4, 1, anchor = c(-1, 0)] <- Class <- gtable(names(data)[!num], 
-    multiple = TRUE, cont = vbox)
+    multiple = TRUE)
 
   # Tour selection column
-  vbox[1, 3, anchor=c(-1,0)] <- "Tour Type"
+  vbox[1, 3, anchor=c(-1, 0)] <- "Tour Type"
   tour_types <- c("Grand", "Little", "Guided(holes)", "Guided(cm)", "Guided(lda_pp)", "Local")
-  vbox[2, 3] <- TourType <- gradio(tour_types, cont=vbox, handler = NULL)
+  vbox[2, 3] <- TourType <- gradio(tour_types)
 
-  # speed slider control
-  vbox[5,1, anchor=c(-1,0)] <- "Speed"
-  vbox[6,1, expand=T] <- sl <- gslider(from = 0, to= 20, by=1, value = 1, 
-    cont = vbox)
+  # speed and pause
+  vbox[5,1, anchor = c(-1, 0)] <- "Speed"
+  vbox[6,1, expand = TRUE] <- sl <- gslider(from = 0, to = 5, by = 0.1, value = 1)
+  
+  vbox[6, 3] <- gcheckbox("Pause", 
+    handler = function(h, ...) pause(svalue(h$obj)))
 
-  #axes control
+  # axes control
   vbox[3,3, anchor=c(-1,0)] <- "Axes Locations"
   locations <- c("center", "bottomleft", "off")
-  vbox[4,3, anchor=c(-1,0)] <- dl <- gradio(locations, cont = vbox)
-
+  vbox[4,3, anchor=c(-1,0)] <- dl <- gradio(locations)
 
   # buttons control
-
-  buttonGroup = ggroup(horizontal = F, cont=vbox)
-  
-  pauseButton = gcheckbox("Pause",cont=buttonGroup, handler = function(h,...){
-    paused <- svalue(pauseButton)
+  pause <- function(paused) {
     if (paused) {
       gtkIdleRemove(anim_id)
     } else {
       anim_id <<- gIdleAdd(draw_frame)
     }
-  }) 
-  addSpace(buttonGroup,10)
-
-  okButton = gbutton("Apply", cont=buttonGroup)
-  addhandlerclicked(okButton, handler = function(h,...) {
-    update_tour()
-  })
-  addSpace(buttonGroup,10)
-
-  # Add animation listener.  This runs draw_frame every 1/33 of a second
-  anim_id <- gIdleAdd(draw_frame)
-
-  quitPushed <<- FALSE
-  quitButton = gbutton("Quit",cont=buttonGroup)
-  addHandlerClicked(quitButton, handler= function(h,...){
-    quitPushed <<- TRUE
-    dispose(w)
-    gtkIdleRemove(anim_id)
-  })
-
-  vbox[4,4, anchor=c(0,1)] = buttonGroup
+  }
+  buttonGroup <- ggroup(horizontal = F, cont=vbox)  
   
+  # addSpace(buttonGroup,10)
+  gbutton("Apply", cont = buttonGroup, handler = update_tour)
+  
+  # addSpace(buttonGroup,10)
+  gbutton("Quit",cont=buttonGroup, handler = function(...) {
+    pause(TRUE)
+    dispose(w)
+  })
+
+  vbox[4:6, 4, anchor = c(0, 1)] <- buttonGroup
   
   # If on a mac, open a Cairo device, if there's not already one open
   # The cairo device has a much better refresh rate than Quartz
@@ -149,5 +93,49 @@ gui_xy <- function(data = flea, ...) {
     CairoX11()
   }
   
+  update_tour()
+  pause(FALSE)
+  visible(w) <- TRUE
   
+  invisible()
+}
+
+
+create_tour <- function(data, var_selected, cat_selected, axes_location, tour_type, aps) {
+  if (length(var_selected) < 3) {
+    gmessage("Please select at least three variables", icon = "warning")
+    return()
+  }
+  
+  # Work out point colours
+  cat <- data[cat_selected]
+  if (length(cat_selected) > 0) {
+    # collapse to single variable if multiple selected
+    int <- interaction(cat, drop = TRUE)
+    pal <- rainbow_hcl(length(levels(int)))
+    col <- pal[as.numeric(int)]
+  } else {
+    col <- "black"
+  }
+  
+  display <- display_xy(data, axes = axes_location, center = TRUE, 
+    col = col)
+
+  # Work out which type of tour to use
+  tour <- switch(tour_type,
+    "Grand" = grand_tour(), 
+    "Little" = little_tour(), 
+    "Guided(holes)" = guided_tour(holes), 
+    "Guided(cm)" = guided_tour(cm), 
+    "Guided(lda_pp)" = guided_tour(), 
+    "Local" = local_tour()
+  )
+  
+      
+  list(
+    data = rescale(data[var_selected]),
+    tour_path = tour,
+    display = display,
+    aps = aps
+  )
 }
